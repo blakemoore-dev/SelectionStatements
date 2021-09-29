c410ba7c952f191b26702acad433d89341be9968
lostinthestack <lostinthestack@protonmail.com>
2021-09-29 09:35:17 -0500

    Initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            
+            Console.Write("Guess the number:    ");
+            
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
